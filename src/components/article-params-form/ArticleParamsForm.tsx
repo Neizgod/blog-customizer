4c743dc28d4f16ca2bb51ef9f76198ca6cbc07bc
Timur Sanatov <neizpro@gmail.com>
2026-04-27 22:22:06 +0300

    Создана форма

--- src/components/article-params-form/ArticleParamsForm.tsx
+++ src/components/article-params-form/ArticleParamsForm.tsx
@@ -2,13 +2,32 @@ import { ArrowButton } from 'src/ui/arrow-button';
 import { Button } from 'src/ui/button';
 
 import styles from './ArticleParamsForm.module.scss';
+import { useReducer, useRef, useState } from 'react';
+import clsx from 'clsx';
+import { RadioGroup } from 'src/ui/radio-group';
+import { Select } from 'src/ui/select';
+import { fontFamilyOptions, fontSizeOptions, fontColors, backgroundColors, contentWidthArr } from 'src/constants/articleProps';
+import { Separator } from 'src/ui/separator';
 
 export const ArticleParamsForm = () => {
+	const [formState, setFormState] = useState(false);
+
 	return (
 		<>
-			<ArrowButton isOpen={false} onClick={() => {}} />
-			<aside className={styles.container}>
+			<ArrowButton
+				isOpen={formState}
+				onClick={() => {
+					setFormState((formState) => !formState);
+				}}
+			/>
+			<aside className={clsx(styles.container, { [styles.container_open]: formState })}>
 				<form className={styles.form}>
+					<Select title='Шрифт' options={fontFamilyOptions} selected={fontFamilyOptions[0]} />
+					<RadioGroup title='Размер шрифта' name='Размер шрифта' options={fontSizeOptions} selected={fontSizeOptions[0]}/>
+					<Select title='Цвет шрифта' options={fontColors} selected={fontColors[0]} />
+					<Separator/>
+					<Select title='Цвет фона' options={backgroundColors} selected={backgroundColors[0]} />
+					<Select title='Ширина контента' options={contentWidthArr} selected={contentWidthArr[0]} />
 					<div className={styles.bottomContainer}>
 						<Button title='Сбросить' htmlType='reset' type='clear' />
 						<Button title='Применить' htmlType='submit' type='apply' />
